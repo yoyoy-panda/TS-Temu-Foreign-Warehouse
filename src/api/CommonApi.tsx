acdc54efb4f965a425a1f547a4dd76c18e471027
Panda <yoyoy.twcp@gmail.com>
2025-08-15 16:06:26 +0800

    copy commonapi from old proj

---
+++ src/api/CommonApi.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+// .env
+const baseURL = process.env.REACT_APP_BLTWCP_BASEPATH!;
+const token = process.env.REACT_APP_BLTWCP_APITOKEN!;
+
+const apiClient = axios.create({
+  baseURL,
+  headers: {
+    token, // Swagger token
+    "Content-Type": "application/json",
+  },
+});
+
+// common error handler
+apiClient.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    console.error("API Error:", err);
+    if (err.response) {
+      console.error("Status:", err.response.status);
+      console.error("Data:", err.response.data);
+    }
+    return Promise.reject(err);
+  }
+);
+
+// common GET
+export async function get<TResponse>(url: string): Promise<TResponse> {
+  const response = await apiClient.get<TResponse>(url);
+  //console.log(response.data);
+  return response.data;
+}
+
+// common POST
+export async function post<TRequest, TResponse>(
+  url: string,
+  data: TRequest
+): Promise<TResponse> {
+  const response = await apiClient.post<TResponse>(url, data);
+  //console.log(url,response.data);
+  return response.data;
+}
+
+// common API response handler
+export function handleApiResponse<T>(response: { content: T }): T {
+  return response.content;
+}
