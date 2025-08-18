5a499ff9956f3a2d422d0588fe959fd934c64e5b
Panda <yoyoy.twcp@gmail.com>
2025-08-18 11:03:56 +0800

    pre-set

---
+++ src/api/RealApi.tsx
@@ -0,0 +1,24 @@
+import { post, get, handleApiResponse } from "./commonApi";
+// Request
+/*
+import {
+  GetFOBListRequest,
+  GetWarehouseListRequest,
+  GetStateByCountryRequest,
+  GetShipMethodListRequest,
+  GetOrderByBatchRequest,
+  SubmitOrderByBatchRequest,
+  UpdateOrderRequest,
+  GetOnHandRequest,
+  ItemListRequest,
+} from "../types/ApiType";
+// Response
+import {
+  ApiResponse,
+  SelectOption,
+  ApiResponseWithOrderTypeItem,
+  OrderApiResponse,
+  OnHandItem,
+  ItemListModel,
+} from "../types/ApiType";
+*/
