a7982372d7d197eeb99906a4eb7821805ea310e8
Panda <yoyoy.twcp@gmail.com>
2025-08-22 16:25:39 +0800

    add sxprops to authmessage

--- src/components/AuthMessage.tsx
+++ src/components/AuthMessage.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Alert } from "@mui/material";
+import type { SxProps, Theme } from "@mui/material";
 
 interface AuthMessageProps {
   message: string | null;
   severity?: "success" | "error" | "info" | "warning" | null;
+  sx?: SxProps<Theme>;
 }
 
-const AuthMessage: React.FC<AuthMessageProps> = ({ message, severity}) => {
+const AuthMessage: React.FC<AuthMessageProps> = ({ message, severity, sx }) => {
   if (!message) return null;
 
   return (
-    <Alert severity={severity === null ? undefined : severity} sx={{ m: 1, width: "100%", maxWidth: "500px" }}>
+    <Alert
+      severity={severity === null ? undefined : severity}
+      sx={{ ...sx, width: "100%" }}
+    >
       {message}
     </Alert>
   );
